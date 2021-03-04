cfb8baeac07b27966dfc4a287a0425d6cb27e59b
mmackiewicz1 <m.mackiewicz@kainos.com>
2021-03-04 12:51:29 +0100

    Updated readme, added env variables to script files.

--- docker/vars.local.sh
+++ docker/vars.local.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env bash
 
 export GPC_CONSUMER_SERVER_PORT="8080"
+export GPC_CONSUMER_GPC_GET_URL="http://localhost:8210"
+export GPC_CONSUMER_GPC_STRUCTURED_PATH="/GP0001/STU3/1/gpconnect/fhir/Patient/$gpc.getstructuredrecord"
