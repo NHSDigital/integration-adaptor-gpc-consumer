8f5b538829a0a09fbc31ac8d6560037d3e3f9a9a
mmackiewicz1 <m.mackiewicz@kainos.com>
2021-03-05 14:05:56 +0100

    Implemented headers and error body logging

---
+++ service/src/main/java/uk/nhs/adaptors/gpc/consumer/LoggingGatewayFilterFactory.java
@@ -0,0 +1,87 @@
+package uk.nhs.adaptors.gpc.consumer;
+
+import java.nio.charset.StandardCharsets;
+import java.util.List;
+
+import org.apache.commons.lang3.StringUtils;
+import org.reactivestreams.Publisher;
+import org.springframework.cloud.gateway.filter.GatewayFilter;
+import org.springframework.cloud.gateway.filter.OrderedGatewayFilter;
+import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
+import org.springframework.core.io.buffer.DataBuffer;
+import org.springframework.core.io.buffer.DataBufferUtils;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.server.reactive.ServerHttpResponse;
+import org.springframework.http.server.reactive.ServerHttpResponseDecorator;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.extern.slf4j.Slf4j;
+import reactor.core.publisher.Mono;
+
+@Component
+@Slf4j
+public class LoggingGatewayFilterFactory extends AbstractGatewayFilterFactory<LoggingGatewayFilterFactory.Config> {
+    private static final List<String> LOGGABLE_HEADER_KEYS = List.of("Ssp-From", "Ssp-To");
+    private static final String LOG_TEMPLATE = "Gateway filter log: %s %s URL: %s";
+    private static final String HEADERS_PREFIX = "Headers: { ";
+    private static final String HEADERS_SUFFIX = "}";
+    private static final String COLON = ": ";
+
+    public LoggingGatewayFilterFactory() {
+        super(Config.class);
+    }
+
+    @Override
+    public GatewayFilter apply(Config config) {
+        return new OrderedGatewayFilter((exchange, chain) -> {
+            LOGGER.info(String.format(LOG_TEMPLATE,
+                config.getBaseMessage(),
+                prepareHeaderLog(exchange.getRequest().getHeaders()),
+                exchange.getRequest().getURI()));
+
+            return chain.filter(exchange.mutate().response(prepareErrorHandlingResponseDecorator(exchange)).build());
+        }, -2);
+    }
+
+    private String prepareHeaderLog(HttpHeaders httpHeaders) {
+        StringBuilder headersLogBuilder = new StringBuilder(HEADERS_PREFIX);
+        LOGGABLE_HEADER_KEYS.forEach(key -> {
+            if (httpHeaders.containsKey(key)) {
+                headersLogBuilder.append(key)
+                    .append(COLON)
+                    .append(httpHeaders.get(key))
+                    .append(StringUtils.SPACE);
+            }
+        });
+        headersLogBuilder.append(HEADERS_SUFFIX);
+
+        return headersLogBuilder.toString();
+    }
+
+    private ServerHttpResponseDecorator prepareErrorHandlingResponseDecorator(ServerWebExchange exchange) {
+        return new ServerHttpResponseDecorator(exchange.getResponse()) {
+            @Override
+            public Mono<Void> writeWith(Publisher<? extends DataBuffer> body) {
+                return DataBufferUtils.join(body)
+                    .flatMap(dataBuffer -> handleError(getDelegate(), dataBuffer));
+            }
+        };
+    }
+
+    private Mono<Void> handleError(ServerHttpResponse response, DataBuffer dataBuffer) {
+        if (response.getStatusCode() != null && !response.getStatusCode().is2xxSuccessful()) {
+            LOGGER.error("An error with status occurred: " + response.getStatusCode());
+            LOGGER.error(StandardCharsets.UTF_8.decode(dataBuffer.asByteBuffer()).toString());
+        }
+        return response.writeWith(Mono.just(dataBuffer));
+    }
+
+    @Setter
+    @Getter
+    public static class Config {
+        private String baseMessage;
+    }
+}
