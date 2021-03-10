53915ea79a97611cd34c48aba07e8df7d65af938
msolecka <m.solecka@kainos.com>
2021-03-10 13:30:26 +0100

    NIAD-1130: Get document proxy (#13)

--- docker/vars.local.sh
+++ docker/vars.local.sh
@@ -2,5 +2,6 @@
 
 export GPC_CONSUMER_SERVER_PORT="8080"
 export GPC_CONSUMER_GPC_GET_URL="http://localhost:8210"
-export GPC_CONSUMER_GPC_GET_PATIENT_PATH="/GP0001/STU3/1/gpconnect/fhir/Patient"
 export GPC_CONSUMER_GPC_STRUCTURED_PATH="/GP0001/STU3/1/gpconnect/fhir/Patient/\$gpc.getstructuredrecord"
+export GPC_CONSUMER_GPC_GET_DOCUMENT_PATH="/GP0001/STU3/1/gpconnect/fhir/Binary/{documentId}"
+export GPC_CONSUMER_GPC_GET_PATIENT_PATH="/GP0001/STU3/1/gpconnect/fhir/Patient"
