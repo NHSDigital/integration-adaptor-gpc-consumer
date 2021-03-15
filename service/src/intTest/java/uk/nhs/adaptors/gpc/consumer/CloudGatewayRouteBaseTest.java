9ba23ccf220162d42f7863279c1014b99644a373
Marcin Mackiewicz <m.mackiewicz@kainos.com>
2021-03-15 10:54:33 +0100

    Niad 1080 lookup gpc endpoints in sds filter (#16)
    * Implemented sds filter
    
    * Pipe does not need to be encoded
    
    * Removed redundant header
    
    * Changed test data
    
    * Fixed tests
    
    * Implemented CR changes
    
    * Removed test correlation id
    
    * Implemented Cr changes
    
    * Environment variable is being taken into account when performing sds request
    
    * Implemented CR changes
    
    * Implemented CR changes
    
    * Removed space
    
    * Implemented CR changes
    
    * Added constains key
    
    * Removed function

--- service/src/intTest/java/uk/nhs/adaptors/gpc/consumer/CloudGatewayRouteBaseTest.java
+++ service/src/intTest/java/uk/nhs/adaptors/gpc/consumer/CloudGatewayRouteBaseTest.java
@@ -1,4 +1,5 @@
 package uk.nhs.adaptors.gpc.consumer;
+
 import java.time.Duration;
 
 import org.junit.jupiter.api.AfterAll;
@@ -18,13 +19,28 @@ public class CloudGatewayRouteBaseTest {
     private static final int WIREMOCK_PORT = 8210;
     private static final int MAX_TIMEOUT = 10;
     private static final String LOCALHOST_URI = "http://localhost:";
-    public static final WireMockServer WIRE_MOCK_SERVER = new WireMockServer(WIREMOCK_PORT);
-    public static final String FHIR_PATIENT_URI = "/GP0001/STU3/1/gpconnect/fhir/Patient";
+    protected static final WireMockServer WIRE_MOCK_SERVER = new WireMockServer(WIREMOCK_PORT);
+    protected static final String GPC_URL_ENVIRONMENT_VARIABLE_NAME = "GP2GP_GPC_GET_URL";
+    protected static final String DOCUMENT_PATIENT_URI = "/GP0001/STU3/1/gpconnect/documents/Patient";
+    protected static final String ENDPOINT = "/Endpoint";
+    protected static final String SSP_FROM_HEADER = "Ssp-From";
+    protected static final String SSP_TO_HEADER = "Ssp-To";
+    protected static final String SSP_INTERACTION_ID_HEADER = "Ssp-InteractionID";
+    protected static final String SSP_TRACE_ID_HEADER = "Ssp-TraceID";
+    protected static final String ANY_STRING = "any";
+    protected static final String EXAMPLE_SDS_BODY = "{\"resourceType\":\"Bundle\","
+        + "\"id\":\"47DBB1CA-256D-410E-B00B-C19C1F13E9F6\","
+        + "\"entry\":[{\"resource\":{\"resourceType\":\"Endpoint\",\"id\":\"307B4278-DFED-4A27-8B51-1539DB1B2C62\","
+        + "\"address\":\"%s/GP0001/STU3/1/gpconnect/\"}}]}";
+    protected static final String EXPECTED_NOT_FOUND_BODY = "{\"resourceType\": \"OperationOutcome\",\"meta\": {\"profile\": "
+        + "[\"https://fhir.nhs.uk/StructureDefinition/gpconnect-operationoutcome-1\" ]},\"issue\": [{\"severity\": \"error\","
+        + "\"code\": \"invalid\",\"details\": {\"coding\":[{\"system\": \"https://fhir.nhs.uk/ValueSet/gpconnect-error-or-warning-code-1\","
+        + "\"code\": \"NO_RECORD_FOUND\",\"display\": \"No Record Found\"}]},\"diagnostics\": \"No record found\"}]}";
 
     @LocalServerPort
     private int port = 0;
     @Getter
-    private WebTestClient webTestClient;;
+    private WebTestClient webTestClient;
     private String baseUri;
 
     @BeforeAll
@@ -49,5 +65,6 @@ public class CloudGatewayRouteBaseTest {
     @AfterEach
     public void tearDown() {
         WIRE_MOCK_SERVER.resetAll();
+        System.clearProperty(GPC_URL_ENVIRONMENT_VARIABLE_NAME);
     }
 }
--- service/src/intTest/java/uk/nhs/adaptors/gpc/consumer/GetDocumentRouteTest.java
+++ service/src/intTest/java/uk/nhs/adaptors/gpc/consumer/GetDocumentRouteTest.java
@@ -7,23 +7,15 @@ import static com.github.tomakehurst.wiremock.client.WireMock.urlPathEqualTo;
 import org.apache.http.HttpStatus;
 import org.junit.jupiter.api.Test;
 
-public class GetDocumentTest extends CloudGatewayRouteBaseTest {
-    private static final String GET_DOCUMENT_URI = "/GP0001/STU3/1/gpconnect/fhir/Binary/07a6483f-732b-461e-86b6-edb665c45510";
+public class GetDocumentRouteTest extends CloudGatewayRouteBaseTest {
+    private static final String GET_DOCUMENT_URI = "/GP0001/STU3/1/gpconnect/documents/Binary/07a6483f-732b-461e-86b6-edb665c45510";
     private static final String EXPECTED_DOCUMENT_BODY = "{\"resourceType\": \"Binary\","
         + "\"id\": \"07a6483f-732b-461e-86b6-edb665c45510\","
         + "\"contentType\": \"application/msword\","
         + "\"content\": \"response content\"}";
-    private static final String NOT_FOUND_GET_DOCUMENT_URI = "/GP0001/STU3/1/gpconnect/fhir/Binary/00000000-732b-461e-86b6-edb665c45510";
-    private static final String EXPECTED_NOT_FOUND_BODY = "{\"resourceType\": \"OperationOutcome\",\"meta\": {\"profile\": "
-        + "[\"https://fhir.nhs.uk/StructureDefinition/gpconnect-operationoutcome-1\" ]},\"issue\": [{\"severity\": \"error\","
-        + "\"code\": \"invalid\",\"details\": {\"coding\":[{\"system\": \"https://fhir.nhs.uk/ValueSet/gpconnect-error-or-warning-code-1\","
-        + "\"code\": \"NO_RECORD_FOUND\",\"display\": \"No Record Found\"}]},\"diagnostics\": \"No record found\"}]}";
-    private static final String SSP_FROM_HEADER = "Ssp-From";
-    private static final String SSP_TO_HEADER = "Ssp-To";
-    private static final String SSP_INTERACTION_ID_HEADER = "Ssp-InteractionID";
-    private static final String SSP_TRACE_ID_HEADER = "Ssp-TraceID";
+    private static final String NOT_FOUND_GET_DOCUMENT_URI =
+        "/GP0001/STU3/1/gpconnect/documents/Binary/00000000-732b-461e-86b6-edb665c45510";
     private static final String DOCUMENT_INTERACTION_ID = "urn:nhs:names:services:gpconnect:documents:fhir:rest:read:binary-1";
-    private static final String ANY_STRING = "any";
 
     @Test
     public void When_MakingRequestForSpecificDocument_Expect_OkResponse() {
@@ -31,6 +23,10 @@ public class GetDocumentTest extends CloudGatewayRouteBaseTest {
             .willReturn(aResponse()
                 .withStatus(HttpStatus.SC_OK)
                 .withBody(EXPECTED_DOCUMENT_BODY)));
+        WIRE_MOCK_SERVER.stubFor(get(urlPathEqualTo(ENDPOINT))
+            .willReturn(aResponse()
+                .withStatus(HttpStatus.SC_OK)
+                .withBody(String.format(EXAMPLE_SDS_BODY, WIRE_MOCK_SERVER.baseUrl()))));
 
         getWebTestClient().get()
             .uri(GET_DOCUMENT_URI)
@@ -51,6 +47,10 @@ public class GetDocumentTest extends CloudGatewayRouteBaseTest {
             .willReturn(aResponse()
                 .withStatus(HttpStatus.SC_NOT_FOUND)
                 .withBody(EXPECTED_NOT_FOUND_BODY)));
+        WIRE_MOCK_SERVER.stubFor(get(urlPathEqualTo(ENDPOINT))
+            .willReturn(aResponse()
+                .withStatus(HttpStatus.SC_OK)
+                .withBody(String.format(EXAMPLE_SDS_BODY, WIRE_MOCK_SERVER.baseUrl()))));
 
         getWebTestClient().get()
             .uri(NOT_FOUND_GET_DOCUMENT_URI)
@@ -63,5 +63,27 @@ public class GetDocumentTest extends CloudGatewayRouteBaseTest {
             .isNotFound()
             .expectBody()
             .json(EXPECTED_NOT_FOUND_BODY);
+    }
+
+    @Test
+    public void When_MakingRequestForSpecificDocument_Given_GpcUrlEnvVariable_Expect_OkResponse() {
+        System.setProperty(GPC_URL_ENVIRONMENT_VARIABLE_NAME, WIRE_MOCK_SERVER.baseUrl());
+
+        WIRE_MOCK_SERVER.stubFor(get(urlPathEqualTo(GET_DOCUMENT_URI))
+            .willReturn(aResponse()
+                .withStatus(HttpStatus.SC_OK)
+                .withBody(EXPECTED_DOCUMENT_BODY)));
+
+        getWebTestClient().get()
+            .uri(GET_DOCUMENT_URI)
+            .header(SSP_FROM_HEADER, ANY_STRING)
+            .header(SSP_TO_HEADER, ANY_STRING)
+            .header(SSP_INTERACTION_ID_HEADER, DOCUMENT_INTERACTION_ID)
+            .header(SSP_TRACE_ID_HEADER, ANY_STRING)
+            .exchange()
+            .expectStatus()
+            .isOk()
+            .expectBody()
+            .json(EXPECTED_DOCUMENT_BODY);
     }
 }
