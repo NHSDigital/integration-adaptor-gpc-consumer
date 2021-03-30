1f35ca6704d5c95db742b76974ca56e07c1aff3e
msolecka <m.solecka@kainos.com>
2021-03-30 15:25:55 +0200

    NIAD-1271: Release 0.0.4, update release information, change port of gpc consumer in docker configuration and healthcheck in relation to work with gp2gp which was on the same port before

--- docker/vars.public.sh
+++ docker/vars.public.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 
-export GPC_CONSUMER_SERVER_PORT="8080"
+export GPC_CONSUMER_SERVER_PORT="8090"
 export GPC_CONSUMER_GPC_GET_URL="https://orange.testlab.nhs.uk"
-export GPC_CONSUMER_URL="http://localhost:8080"
-
+export GPC_CONSUMER_URL="http://localhost:8090"
