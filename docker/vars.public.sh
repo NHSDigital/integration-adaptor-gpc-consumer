b085d7d55df67c67d868401ebfe53aec03af4bbb
Broom94 <42891767+Broom94@users.noreply.github.com>
2021-03-30 11:30:51 +0100

    add sds sandbox to vars

--- docker/vars.public.sh
+++ docker/vars.public.sh
@@ -3,4 +3,4 @@
 export GPC_CONSUMER_SERVER_PORT="8080"
 export GPC_CONSUMER_GPC_GET_URL="https://orange.testlab.nhs.uk"
 export GPC_CONSUMER_URL="http://localhost:8080"
-
+export GPC_CONSUMER_SDS_URL="https://sandbox.api.service.nhs.uk/spine-directory/"
