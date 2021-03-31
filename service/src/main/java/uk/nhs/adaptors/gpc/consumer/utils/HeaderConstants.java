cc71f86e7454160756db8e54546ff7f345358d0b
ElliotB-96 <elliotb@kainos.com>
2021-03-31 14:11:20 +0100

    Styling fix

--- service/src/main/java/uk/nhs/adaptors/gpc/consumer/utils/HeaderConstants.java
+++ service/src/main/java/uk/nhs/adaptors/gpc/consumer/utils/HeaderConstants.java
@@ -3,5 +3,5 @@ package uk.nhs.adaptors.gpc.consumer.utils;
 public final class HeaderConstants {
     public static final String SSP_TRACE_ID = "Ssp-TraceID";
 
-    private HeaderConstants() {}
+    private HeaderConstants() { }
 }
