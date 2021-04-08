d10c98aa26b88bf00d0f33f18a8a9c0ab5fb40c9
Broom94 <42891767+Broom94@users.noreply.github.com>
2021-04-08 09:17:51 +0100

    logging to debug pem error

--- service/src/main/java/uk/nhs/adaptors/gpc/consumer/filters/SslContextBuilderWrapper.java
+++ service/src/main/java/uk/nhs/adaptors/gpc/consumer/filters/SslContextBuilderWrapper.java
@@ -25,21 +25,25 @@ public class SslContextBuilderWrapper {
     private String subCert;
 
     public SslContextBuilderWrapper clientKey(String clientKey) {
+        LOGGER.info(String.format("DEPLOYMENT DEBUGGING, key:'%s'", clientKey));
         this.clientKey = toPem(clientKey);
         return this;
     }
 
     public SslContextBuilderWrapper clientCert(String clientCert) {
+        LOGGER.info(String.format("DEPLOYMENT DEBUGGING, cert:'%s'", clientCert));
         this.clientCert = toPem(clientCert);
         return this;
     }
 
     public SslContextBuilderWrapper rootCert(String rootCert) {
+        LOGGER.info(String.format("DEPLOYMENT DEBUGGING, root:'%s'", rootCert));
         this.rootCert = toPem(rootCert);
         return this;
     }
 
     public SslContextBuilderWrapper subCert(String subCert) {
+        LOGGER.info(String.format("DEPLOYMENT DEBUGGING, sub:'%s'", subCert));
         this.subCert = toPem(subCert);
         return this;
     }
