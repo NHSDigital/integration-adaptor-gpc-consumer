ddaac5e1e46c6c8d6e19f76e63cbdb5f330ef5f1
Matt Dooner <mattd@kainos.com>
2021-06-15 16:07:43 +0100

    config updates

--- docker/vars.local.sh
+++ docker/vars.local.sh
@@ -1,5 +1,8 @@
+#!/usr/bin/env bash
 export GPC_CONSUMER_SERVER_PORT="8090"
-export GPC_CONSUMER_OVERRIDE_GPC_PROVIDER_URL="http://gpcc-mocks:8110"
+export GPC_CONSUMER_OVERRIDE_GPC_PROVIDER_URL="http://gpcc-mocks:8080"
 export GPC_CONSUMER_URL="http://localhost:8090"
-
-export GPC_ENABLE_SDS="false" 
+export GPC_CONSUMER_SDS_URL="http://gpcc-mocks:8080/spine-directory/"
+export GPC_ENABLE_SDS="true"
+export GPC_CONSUMER_SDS_APIKEY="anykey"
+export GPC_CONSUMER_LOGGING_LEVEL="DEBUG"
